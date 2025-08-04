cc294227549f4e62721a630ddb7741799cbd6db2
rodrigo <rodrigocaceres@rodrigos-MacBook-Pro.local>
2025-08-03 23:03:22 -0400

    refactor

--- src/main/java/com/abacus/portafolio/operation/controller/PortfolioOperationController.java
+++ src/main/java/com/abacus/portafolio/operation/controller/PortfolioOperationController.java
@@ -4,17 +4,20 @@ import com.abacus.portafolio.evolution.dto.PortfolioEvolutionDTO;
 import com.abacus.portafolio.operation.dto.PortfolioOperationDTO;
 import com.abacus.portafolio.operation.service.PortfolioOperationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/portfolio")
 @RequiredArgsConstructor
+@Slf4j
 public class PortfolioOperationController {
     private final PortfolioOperationService portfolioOperationService;
 
     @PostMapping("/{portfolioId}/operation")
     public ResponseEntity<?> processOperation(@PathVariable Long portfolioId, @RequestBody PortfolioOperationDTO request) {
+        log.info("Portfolio operation processing request {}", request);
         PortfolioEvolutionDTO response = portfolioOperationService.process(portfolioId, request);
         return ResponseEntity.ok().body(response);
     }
